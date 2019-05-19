206e1a5c9f2cf2612a54fa143aed78440cfd82cd
Hendri Pretorius <pretorh@gmail.com>
2019-05-19 12:50:08 +0200

    setup credentials and service
    add publisher script, load email and keyfile path from env variables,
    loads the key file and builds the service

---
+++ publisher.py
@@ -0,0 +1,22 @@
+import os
+import httplib2
+from oauth2client.service_account import ServiceAccountCredentials
+from apiclient.discovery import build
+
+SERVICE_ACCOUNT_EMAIL = os.environ['SERVICE_ACCOUNT_EMAIL']
+KEY_FILE = os.environ['KEY_FILE']
+SCOPE = 'https://www.googleapis.com/auth/androidpublisher'
+
+def main():
+    print('setup credentials')
+    # build service acount using p12 file, based on
+    # https://stackoverflow.com/a/35666374/1016377
+    credentials = ServiceAccountCredentials.from_p12_keyfile(SERVICE_ACCOUNT_EMAIL, KEY_FILE, scopes=[SCOPE])
+
+    print('setup service')
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+    service = build('androidpublisher', 'v3', http=http)
+
+if __name__ == '__main__':
+    main()
