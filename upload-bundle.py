cd1302450254e46eabae7c1f9cdf0a28b18c713b
Hendri Pretorius <pretorh@gmail.com>
2019-06-26 23:01:11 +0200

    upload: validate and commit edit after upload

--- upload-bundle.py
+++ upload-bundle.py
@@ -7,6 +7,7 @@ def upload():
     service = publisher.build_service()
     edit_id = publisher.create_edit(service)
     publisher.upload_bundle(service, edit_id, apk_file)
+    publisher.validate_and_commit_edit(service, edit_id)
     print('bye')
 
 if __name__ == '__main__':
